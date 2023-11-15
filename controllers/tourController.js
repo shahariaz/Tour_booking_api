1fcebac305f9448446a8c38bfe038c1fac2bcce4
shahariaz <shahariaz.info@gmail.com>
2023-11-16 02:00:45 +0600

    advanced filter method to the api

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,47 @@ const Tours = require('../models/tour.model');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tours.find();
+    //1A,,,,BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFileds = ['page', 'sort', 'limit', 'fields'];
+    excludedFileds.forEach((el) => delete queryObj[el]);
+    //1b,,,,more advanced
+    console.log(queryObj);
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(queryStr);
+    let query = Tours.find(JSON.parse(queryStr));
+
+    //2 Sorting
+    if (req.query.sort) {
+      console.log(req.query.sort);
+      const sortBy = req.query.sort.split(',').join(' ');
+      console.log(sortBy);
+      query = query.sort(req.query.sort);
+    } else {
+      query = query.sort('-createdAt');
+    }
+    //3))fileds limiting
+    if (req.query.fileds) {
+      const fileds = req.query.fileds.split(',').join(' ');
+      query = query.select('fileds');
+    } else {
+      query = query.select('-__v');
+    }
+
+    //4)Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 1;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+    if (req.query.page) {
+      const numTours = await Tours.countDocuments();
+      if (skip >= numTours) throw new Error('This page not dose not exits');
+    }
+    //5)
+    //execute query
+
+    const tours = await query;
     res.status(200).json({
       status: 'success',
       totalTours: Tours.length,
@@ -11,6 +51,7 @@ exports.getAllTours = async (req, res) => {
       },
     });
   } catch (error) {
+    limitlimit;
     res.status(404).json({
       status: '404 Not Found',
       message: 'Could not find any tour',
@@ -18,16 +59,22 @@ exports.getAllTours = async (req, res) => {
   }
 };
 
-exports.getTour = (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1;
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
+exports.getTour = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const tour = await Tours.findById(id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      statusbar: 'error',
+      message: 'Could not found tour',
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
@@ -49,18 +96,34 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
+exports.updateTour = async (req, res) => {
+  const id = req.params.id;
+  try {
+    updatedTour = await Tours.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+  } catch (error) {}
   res.status(200).json({
     status: 'success',
     data: {
-      tour: '<Updated tour here...>',
+      tour: updateTour,
     },
   });
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Tours.findByIdAndDelete(id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (error) {
+    res.status(404).json({
+      staus: 'failed',
+      message: error,
+    });
+  }
 };
--- models/tour.model.js
+++ models/tour.model.js
@@ -8,17 +8,53 @@ const tourSchema = new Schema({
     unique: true,
     trim: true,
   },
-  rating: {
+  duration: {
     type: Number,
-    required: true,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a maximum group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
+    type: Number,
+
+    default: 4.0,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     default: 1,
   },
-  dis: {
-    type: String,
+  priceDiscount: {
+    type: Number,
   },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 // delete mongoose.connection.models['Tour'];
 const Tours = model('Tours', tourSchema);
