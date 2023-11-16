7802a040cc6a9f74346669de773beff2c8529e3a
shahariaz <shahariaz.info@gmail.com>
2023-11-16 19:40:12 +0600

    starting error handeling

--- app.js
+++ app.js
@@ -9,5 +9,10 @@ app.use(express.json());
 //user routes
 app.use('/api/v1/users', usersRouter);
 app.use('/api/v1/tours', tourRouter);
-
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: '404 Not Found',
+    massage: `Can't find ${req.originalUrl} on this server`,
+  });
+});
 module.exports = app;
