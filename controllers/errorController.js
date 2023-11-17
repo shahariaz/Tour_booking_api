b953a3ae03f854ee8bf313e967a56b8b570dcc36
shahariaz <shahariaz.info@gmail.com>
2023-11-17 07:14:41 +0600

    fixed bug on code and improve errorhandeler

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,16 @@
+const AppError = require('../utils/appError');
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateKeyError = (err) => {
+  const duplicatedField = Object.keys(err.keyValue)[0];
+  const duplicatedValue = err.keyValue[duplicatedField];
+
+  const message = `Duplicate key error: ${duplicatedField} with value ${duplicatedValue}.`;
+  return new AppError(message, 400); // You can choose an appropriate status code
+};
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -7,17 +20,35 @@ const sendErrorDev = (err, res) => {
   });
 };
 const sendErrorProd = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  //Operational, trusted error:send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    //Programing  or unknown error:don't leak error details to client
+  } else {
+    //1Log error
+    console.error('ERROR', err);
+    //2)Send genaric error message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong',
+    });
+  }
 };
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  if (process.env.NODE_ENV !== 'production') {
+  if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
-  } else if (process.env.NODE_ENV !== 'development') {
-    sendErrorProd(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    console.log(error.name);
+    if (err.name === 'CastError') error = handleCastErrorDB(error);
+    if (err.code === 11000) error = handleDuplicateKeyError(error);
+
+    sendErrorProd(error, res);
   }
 };
